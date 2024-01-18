1a5a82ca793bab96d5dc394669fc391013e5ec66
matheusstyt <matheus20galdino@gmail.com>
2024-01-18 12:43:20 -0400

    refactor: page_size > 5.

--- api_ecommerce/products/views.py
+++ api_ecommerce/products/views.py
@@ -23,7 +23,7 @@ class PriceHistoryViewSet(ModelViewSet):
     permission_classes= [IsAuthenticated]
 
 class ProductPagination(PageNumberPagination):
-    page_size = 2
+    page_size = 5
     page_size_query_param = 'page_size'
     max_page_size = 100
     
